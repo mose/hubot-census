44dc07ce545bad5e739d600c4f1b0ced19bfd3e1
mose <mose@mose.com>
2017-06-03 05:52:09 +0800

    add hubot-script (without s) tag

--- get.sh
+++ get.sh
@@ -2,10 +2,12 @@
 
 test -d tmp || mkdir tmp
 
+test -f tmp/hubot-scripts.json || curl -s 'https://skimdb.npmjs.com/registry/_design/app/_view/byKeyword?startkey=\["hubot-scripts"\]&endkey=\["hubot-script",\{\}\]&group_level=3' > tmp/hubot-script.json
 test -f tmp/hubot-scripts.json || curl -s 'https://skimdb.npmjs.com/registry/_design/app/_view/byKeyword?startkey=\["hubot-scripts"\]&endkey=\["hubot-scripts",\{\}\]&group_level=3' > tmp/hubot-scripts.json
 test -f tmp/hubot.json || curl -s 'https://skimdb.npmjs.com/registry/_design/app/_view/byKeyword?startkey=\["hubot"\]&endkey=\["hubot",\{\}\]&group_level=3' > tmp/hubot.json
 
 test -f tmp/full.txt || \
+	(cat tmp/hubot-script.json | jq '.rows[].key[1]' | sed -e 's/"//g' > tmp/full.txt) && \
 	(cat tmp/hubot-scripts.json | jq '.rows[].key[1]' | sed -e 's/"//g' > tmp/full.txt) && \
 	(cat tmp/hubot.json | jq '.rows[].key[1]' | sed -e 's/"//g' >> tmp/full.txt)
 
