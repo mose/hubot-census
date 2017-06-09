1e5c5738537753c7cf222320a875c56198fb6d15
mose <mose@mose.com>
2017-06-10 03:30:56 +0800

    add depends

--- bin/get.sh
+++ bin/get.sh
@@ -2,6 +2,7 @@
 
 STORE_DIR="tmp"
 PACKAGES="hubot-script hubot-scripts hubot"
+DEPENDS="hubot"
 
 test -d $STORE_DIR || mkdir $STORE_DIR
 
@@ -11,12 +12,22 @@ do
     curl -s "https://skimdb.npmjs.com/registry/_design/app/_view/byKeyword?startkey=\[\"$package\"\]&endkey=\[\"$package\",\{\}\]&group_level=3" > $STORE_DIR/$package.json
 done
 
+for depends in $DEPENDS
+do
+  test -f $STORE_DIR/$depends.dep.json || \
+    curl -s "https://skimdb.npmjs.com/registry/_design/app/_view/dependentVersions?startkey=\[\"$depends\"\]&endkey=\[\"$depends\",\{\}\]&reduce=false" > $STORE_DIR/$depends.dep.json
+done
+
 if ! test -f $STORE_DIR/full.txt
 then
   echo "refresh full"
   for package in $PACKAGES
   do
     cat $STORE_DIR/$package.json | jq '.rows[].key[1]' | sed -e 's/"//g' >> $STORE_DIR/full.txt
+  done
+  for depends in $DEPENDS
+  do
+    cat $STORE_DIR/$depends.dep.json | jq '.rows[].id' | sed -e 's/"//g' >> $STORE_DIR/full.txt
   done
 fi
 
