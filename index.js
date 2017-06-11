de389a4e964a7ab4d4f35eab66cb0e4e59a2be1b
mose <mose@mose.com>
2017-06-11 14:58:56 +0800

    prepare static page service

---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+}
+
+walk('tmp/packages', function(err, results) {
+  if (err) {
+    console.log("err: " + err);
+  } else {
+    console.log("results: " + util.inspect(results));
+  }
+});
