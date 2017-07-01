264ce678a2cdaced41ba75bdd0b67700c90ffce0
mose <mose@mose.com>
2017-07-01 15:54:24 +0800

    display recent

--- docs/js/packages.js
+++ docs/js/packages.js
@@ -1,7 +1,30 @@
 "use strict";
 
+const showRecent = function(selector, data) {
+  console.log(data);
+  const div = document.querySelector(selector);
+  for (var el in data) {
+    const divlist = document.createElement('div');
+    divlist.className = "package";
+    div.appendChild(divlist);
+    divlist.insertAdjacentHTML("beforeend", "<h3>" + data[el].name + ' <span>' + data[el]["dist-tags"].latest + '</span></h3>\n');
+    divlist.insertAdjacentHTML("beforeend", "<div>" + data[el].description + '</div>\n');
+    divlist.insertAdjacentHTML("beforeend", "<div class=\"date\">" + data[el].time[data[el]["dist-tags"].latest] + '</div>\n');
+  }
+}
+
+const getPackage = function(pack) {
+  return fetch('data/packages/' + pack + '.json').then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok.');
+  });
+}
+
+let payload = {};
 fetch("data/all_packages.json").then(function(response) {
-  if(response.ok) {
+  if (response.ok) {
     return response.json();
   }
   throw new Error('Network response was not ok.');
@@ -9,18 +32,32 @@ fetch("data/all_packages.json").then(function(response) {
   const today = new Date();
   const lastDay = today.setDate(today.getDate() - 1);
   const lastWeek = today.setDate(today.getDate() - 7);
-  return packages.reduce(function(acc, el) {
+  payload = packages.reduce(function(acc, el) {
     let lastRelease = el.releases[el.releases.length - 1];
     let lastReleaseDate = new Date(lastRelease);
     if (lastReleaseDate.getTime() > lastDay) {
-      acc.today[el.id] = lastRelease;
+      acc.today.push({ id: el.id, date: lastRelease});
     } else if (lastReleaseDate.getTime() > lastWeek) {
-      acc.pastWeek[el.id] = lastRelease;
+      acc.pastWeek.push({ id: el.id, date: lastRelease});
     }
     return acc;
-  }, { today: {}, pastWeek: {} });
+  }, { today: [], pastWeek: [] });
+  payload.today = payload.today.sort(function(a, b) {
+    return - a.date.localeCompare(b.date);
+  });
+  payload.pastWeek = payload.pastWeek.sort(function(a, b) {
+    return - a.date.localeCompare(b.date);
+  });
+  return 'ok';
 }).then(function(data) {
-  console.log(data);
+  payload.today = payload.today.map(e => getPackage(e.id));
+  payload.pastWeek = payload.pastWeek.map(e => getPackage(e.id));
+  return Promise.all(payload.today);
+}).then(function(data) {
+  showRecent("div.recent", data);
+  return Promise.all(payload.pastWeek);
+}).then(function(data) {
+  showRecent("div.pastweek", data);
 }).catch(function(error) {
   console.log('Error: ' + error.message);
 });
