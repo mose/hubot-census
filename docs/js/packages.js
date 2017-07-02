4687231445be2a383a8f01c1d94b126fdd198993
mose <mose@mose.com>
2017-07-02 12:18:46 +0800

    add npm link

--- docs/js/packages.js
+++ docs/js/packages.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const githubicon = '<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>';
+const npmicon = '<svg viewBox="0 0 32 32" version="1.1" width="16" height="16" aria-hidden="true"><path d="M0 0v32h32v-32h-32zM26 26h-4v-16h-6v16h-10v-20h20v20z"></path></svg>'
 
 const showRecent = function(selector, data) {
   const div = document.querySelector(selector);
@@ -10,8 +11,9 @@ const showRecent = function(selector, data) {
     divlist.className = "package";
     div.appendChild(divlist);
     divlist.insertAdjacentHTML("beforeend", "<h3>" + data[el].name + ' <span>' + data[el]["dist-tags"].latest + '</span></h3>');
+    divlist.insertAdjacentHTML("beforeend", '<a href="https://www.npmjs.com/package/' + data[el].name + '" class="icon npm" target="_new">' + npmicon + '</a>');
     if (data[el].homepage) {
-      divlist.insertAdjacentHTML("beforeend", '<a href="' + data[el].homepage + '" class="icon" target="_new">' + githubicon + '</a>');
+      divlist.insertAdjacentHTML("beforeend", '<a href="' + data[el].homepage + '" class="icon github" target="_new">' + githubicon + '</a>');
     }
     divlist.insertAdjacentHTML("beforeend", "<div class=\"description\">" + data[el].description + '</div>');
     const releasedate = new Date(data[el].time[data[el]["dist-tags"].latest]);
